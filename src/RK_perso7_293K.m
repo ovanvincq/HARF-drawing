710c7fe3ecea3eb228a770d2a98f7cc6c0022bd1
Olivier Vanvincq <olivier.vanvincq@univ-lille.fr>
2025-03-24 12:14:50 +0100

    Update to v1.4

--- src/RK_perso7_293K.m
+++ src/RK_perso7_293K.m
@@ -18,7 +18,8 @@ function dydt = RK_perso7_293K(param,t,y,Pcore,DeltaP)
     dydt(1)=param.rho(T)*pi*(R^2-r^2)*(w*dwdz-param.g)-param.gamma(T)*pi*(dydt(3)-R^2*dwdz)/(2*R*w)-param.gamma(T)*pi*(dydt(3)-r^2*dwdz)/(2*r*w);
     dydt(2)=dwdz;
     dydt(4)=dydt(3);
-    dydt(5)=(R*param.N*(Ta-T)*2/(R^2-r^2)+param.sigma*param.alpha(T)*(Ta^4-T^4))/(param.rho(T)*param.Cp(T)*w);
+    %dydt(5)=(R*param.N*(Ta-T)*2/(R^2-r^2)+param.sigma*param.alpha(T)*(Ta^4-T^4))/(param.rho(T)*param.Cp(T)*w);
+    dydt(5)=(R*param.N*(Ta-T)+R*param.sigma*(param.alpha(Ta)*Ta^4-param.alpha(T)*T^4))*2/(R^2-r^2)/(param.rho(T)*param.Cp(T)*w);
     dydt(6)=(DeltaP*rc^2*Rc^2-param.gamma(T)*rc*Rc*(rc+Rc))/(mu*(Rc^2-rc^2));         
     dydt(7)=dydt(6);
 end
